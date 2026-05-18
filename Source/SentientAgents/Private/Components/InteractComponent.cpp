b4afd0a80c9d2f8f6d0868306d332ad689f51df6
Daniele Malusardi <mdani1102@gmail.com>
2026-05-18 17:57:27 +0200

    Something similar to an interactable system

---
+++ Source/SentientAgents/Private/Components/InteractComponent.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/Components/InteractComponent.h"
+
+#include "Interfaces/Interactable.h"
+
+
+UInteractComponent::UInteractComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UInteractComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetSphereRadius(InteractionSphereRadius);
+	//SetCollisionResponseToAllChannels(ECR_Ignore);
+	//SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SetGenerateOverlapEvents(true);
+	
+	OnComponentBeginOverlap.AddDynamic(this, &UInteractComponent::SpawnUI);
+	OnComponentEndOverlap.AddDynamic(this, &UInteractComponent::DespawnUI);
+}
+
+void UInteractComponent::SpawnUI(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//UI spawn logic on single actor 
+	
+	if (OtherActor->Implements<UInteractable>())
+	{
+		
+		
+		
+	}
+	
+}
+
+void UInteractComponent::DespawnUI(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	//UI despawn logic
+}
+
---
+++ Source/SentientAgents/Public/Components/InteractComponent.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "InteractComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SENTIENTAGENTS_API UInteractComponent : public USphereComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float InteractionSphereRadius = 0.0f;
+
+	UInteractComponent();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	UFUNCTION()
+	void SpawnUI(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void DespawnUI(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+};
