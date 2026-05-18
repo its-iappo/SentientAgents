56bba287d4ee853519552f4eba423a4c6f906c58
Ezio_auditore1 <diegomimini.giochi@gmail.com>
2026-05-18 16:56:59 +0200

    BP_HealthTestCharacter , LV_HealthTest
    added C++ Child HealthTestCharacter from SentientAgentsCharacter and BP_HealthTestCharacter,

---
+++ Source/SentientAgents/Public/HealthTestCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SentientAgentsCharacter.h"
+#include "HealthTestCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SENTIENTAGENTS_API AHealthTestCharacter : public ASentientAgentsCharacter
+{
+	GENERATED_BODY()
+	
+};
