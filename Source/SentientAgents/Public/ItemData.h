9d851959a30442e1c0b056afed5246f5454c2d76
Gabriele22323 <Gabriele.gianrosso@gmail.com>
2026-05-18 17:56:47 +0200

    Added ItemData struct

---
+++ Source/SentientAgents/Public/ItemData.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Class.h"
+#include "ItemData.generated.h"
+
+/**
+ * 
+ */
+USTRUCT()
+struct SENTIENTAGENTS_API FItemData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, meta=(AllowPrivateAccess))
+	int ID = 0;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, meta=(AllowPrivateAccess))
+	int Quantity = 1;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, meta=(AllowPrivateAccess))
+	FString Name = "";
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, meta=(AllowPrivateAccess))
+	UTexture2D* Icon;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, meta=(AllowPrivateAccess))
+	bool bIsStackable = false;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, meta=(AllowPrivateAccess))
+	float Damage = 0;
+};
+
+
+
