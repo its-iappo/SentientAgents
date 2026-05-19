50fe357914c3a93b0099fc530a4d97eb0df757c4
Daniele Malusardi <mdani1102@gmail.com>
2026-05-19 14:35:11 +0200

    Fixed issue

--- Source/SentientAgents/Private/Interfaces/InteractionSystem/Interactable.cpp
+++ Source/SentientAgents/Private/Interfaces/InteractionSystem/Interactable.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Public/Interfaces/Interactable.h"
+#include "Interfaces/InteractionSystem/Interactable.h"
 
 
 // Add default functionality here for any IInteractable functions that are not pure virtual.
